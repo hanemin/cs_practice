eb2297ba899e28198e01fd3c66a46296e35de4fe
TakagiTakuro <bgdaewalkman@gmail.com>
2017-05-03 23:02:35 +0900

    @4

---
+++ @4.cs
@@ -0,0 +1,31 @@
+using System;
+namespace _4 {
+    //1<=n<=20
+    //-1e8<=a_i<=1e8
+    //-1e8<=k<=1e8
+    class Program {
+        static int n, k;
+        static int[] a;
+        static void Solve() {
+            if (dfs(0, 0))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("False");
+        }
+        static bool dfs(int i,int sum) {
+            if (i == a.Length) return (sum == k);
+            //not add
+            if(dfs(i + 1, sum)) return true;
+            //add
+            if(dfs(i + 1, sum + a[i])) return true;
+            //muri
+            return false;
+        }
+        static void Main(string[] args) {
+            n = int.Parse(Console.ReadLine());
+            a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            k = int.Parse(Console.ReadLine());
+            Solve();
+        }
+    }
+}
